3d2e90ea53753732ee3959fc241f2790b2fd4f3e
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-05-28 16:20:42 -0300

    las commit

--- Biblioteca.c
+++ Biblioteca.c
@@ -7,12 +7,12 @@ int main()
 {
   setlocale(LC_ALL, "pt_BR_uft8");
   printf("###### Cartório da EBAC ######\n\n");
-  printf("Escolha a opção desejada do menu: \n");
-  printf("\t\t1 - Registrar nomes\n");
-  printf("\t\t2 - Consultar nomes\n");
-  printf("\t\t3 - Deletar nomes\n");
+  printf("Olá, seja bem-vindo(a)!\nEscolha a opção desejada do menu:\n");
+  printf("\t\t1 - Registrar nomes;\n");
+  printf("\t\t2 - Consultar nomes;\n");
+  printf("\t\t3 - Deletar nomes.\n");
 
 
-  printf("\n\nEsse software é de livre uso dos alunos da EBAC\n");
+  printf("\n\n##Esse software é de livre uso dos alunos da EBAC.##\n");
   return 0;
 }
