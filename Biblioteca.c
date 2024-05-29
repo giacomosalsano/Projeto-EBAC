dfbdc5ba239686fd7f42d88c24af423c5654c6e7
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-05-29 15:43:33 -0300

    no comments

--- Biblioteca.c
+++ Biblioteca.c
@@ -1,57 +1,75 @@
 #include <stdio.h> //Biblioteca de comunicação com o Usuário
 #include <stdlib.h> //Biblioteca de alocação de espaço e memória
 #include <locale.h> //Biblioteca de alocações de texto por região
+#include <string.h>
 
 
 
 int main()
 {
   int opcaoUsuario=0;
+  int laco=1;
+  char* nomeDoUdsuario;
+
+  
 
 
-  setlocale(LC_ALL, "pt_BR_uft8");
+  for(laco=1;laco==1;) {
+
+    // system("clear"); //Limpar a tela 
+
+    setlocale(LC_ALL, "pt_BR_uft8");
 
 
-  printf("##Esse software é de livre uso dos alunos da EBAC.##\n");
-  printf("\n###### Cartório da EBAC ######\n\n");
+    printf("##Esse software é de livre uso dos alunos da EBAC.##\n");
+    printf("\n###### Cartório da EBAC ######\n\n");
 
-  printf("Olá, seja bem-vindo(a)!\nEscolha a opção desejada do menu:\n");
-  printf("\t1 - Registrar nomes;\n");
-  printf("\t2 - Consultar nomes;\n");
-  printf("\t3 - Deletar nomes;\n");
-  printf("\t4 - Sair.\n");
+    printf("Olá, seja bem-vindo(a)!\nEscolha a opção desejada do menu:\n");
+    printf("\t1 - Registrar nomes;\n");
+    printf("\t2 - Consultar nomes;\n");
+    printf("\t3 - Deletar nomes;\n");
+    printf("\t4 - Sair.\n");
 
-  printf("\nOpção: ");
-  scanf("%d", &opcaoUsuario); //Armazenar na variável "opcaoUsuario"
-  // %d to accept input of integers.
-  // %ld to  accept input of long integers
-  // %lld to accept input of long long integers
-  // %f to accept input of real number.
-  // %c to accept input of character types.
-  // %s to accept input of a string.
+    printf("\nOpção: ");
+    scanf("%d", &opcaoUsuario); //Armazenar na variável "opcaoUsuario"
+    // %d to accept input of integers.
+    // %ld to  accept input of long integers
+    // %lld to accept input of long long integers
+    // %f to accept input of real number.
+    // %c to accept input of character types.
+    // %s to accept input of a string.
 
-  // system("cls"); //Limpar a tela
+    switch(opcaoUsuario) {
+      case 1:
+        system("clear"); //Limpar a tela
+        printf("Opção escolhida: Registrar nomes.\n");
+        laco=0;
+        break;
+      case 2:
+        system("clear"); //Limpar a tela
+        printf("Opção escolhida: Consultar nomes.\n");
+        laco=0;
+        break;
+      case 3:
+        system("clear"); //Limpar a tela
+        printf("Opção escolhida: Deletar nomes.\n");
+        laco=0;
+        break;
+      case 4: 
+        system("clear"); //Limpar a tela
+        printf("Opção escolhida: Sair.\n");
+        laco=0;
+        break;
+      default:
+        system("clear"); //Limpar a tela
+        printf("Opção inválida.\n");
+        laco=0;
+        break;
+    }
 
-  if(opcaoUsuario==1) { //Registrar Nomes
-    printf("\nVocê escolheu a opção 1 - Registrar nomes.\n");
   }
 
-  if(opcaoUsuario==2) { //Consultar Nomes
-    printf("\nVocê escolheu a opção 2 - Consultar nomes.\n");
-  }
 
-  if(opcaoUsuario==3) { //Deletar Nomes
-    printf("\nVocê escolheu a opção 3 - Deletar nomes.\n");
-  }
-
-  if(opcaoUsuario==4) { //Sair
-    printf("\nVocê escolheu a opção 4 - Sair.\nObrigado por utilizar nosso serviço.");
-  }
-
-  if(opcaoUsuario < 1 || opcaoUsuario > 4) { //Opção Inválida
-    printf("\nDigite uma opção válida");
-  }
-
-  return 0;
+  // return 0;
 }
 
